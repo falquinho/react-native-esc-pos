b2f8e0a116c4e6fed87038d1ca090cd4818876e4
Lucas Falcão <lucas.falcao.nb@gmail.com>
2020-09-23 11:19:34 -0300

    Set DEFAULT_IMG_WIDTH_OFFSET to 0 (I don't understand this const). Exposes method to explicit set a width offset for anyone that needs this.

--- android/src/main/java/leesiongchan/reactnativeescpos/PrinterService.java
+++ android/src/main/java/leesiongchan/reactnativeescpos/PrinterService.java
@@ -20,6 +20,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
+import java.lang.Math;
 import leesiongchan.reactnativeescpos.helpers.EscPosHelper;
 import leesiongchan.reactnativeescpos.utils.BitMatrixUtils;
 import static io.github.escposjava.print.Commands.*;
@@ -32,7 +33,7 @@ public class PrinterService {
     private LayoutBuilder layoutBuilder = new LayoutBuilder();
     private final int DEFAULT_QR_CODE_SIZE = 200;
     private final int DEFAULT_IMG_MAX_HEIGHT = 200;
-    private final int DEFAULT_IMG_WIDTH_OFFSET = 100;
+    private final int DEFAULT_IMG_WIDTH_OFFSET = 0;
     private int printingWidth = PRINTING_WIDTH_58_MM;
     private io.github.escposjava.PrinterService basePrinterService;
     private ReactApplicationContext context;
@@ -116,8 +117,18 @@ public class PrinterService {
         printImage(readImage(filePath, context));
     }
 
+    public void printImage(String filePath, int widthOffset) throws IOException {
+        printImage(readImage(filePath, context), widthOffset);
+    }
+
     public void printImage(Bitmap image) throws IOException {
         image = EscPosHelper.resizeImage(image, printingWidth - DEFAULT_IMG_WIDTH_OFFSET, DEFAULT_IMG_MAX_HEIGHT);
+        ByteArrayOutputStream baos = generateImageByteArrayOutputStream(image);
+        write(baos.toByteArray());
+    }
+
+    public void printImage(Bitmap image, int widthOffset) throws IOException {
+        image = EscPosHelper.resizeImage(image, Math.max(printingWidth - Math.abs(widthOffset), 0), DEFAULT_IMG_MAX_HEIGHT);
         ByteArrayOutputStream baos = generateImageByteArrayOutputStream(image);
         write(baos.toByteArray());
     }
